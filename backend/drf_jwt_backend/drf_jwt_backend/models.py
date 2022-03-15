472aa3448a5ccc7b1e706f350ef67e0d0b64cc58
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-03-15 17:59:36 -0400

    feat: created Review model in models.py

--- backend/drf_jwt_backend/drf_jwt_backend/models.py
+++ backend/drf_jwt_backend/drf_jwt_backend/models.py
@@ -29,3 +29,13 @@ class CheckIn(models.Model):
 
     class Meta:
         abstract = True
+
+class Review(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    passenger = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_reviewer = models.BooleanField(default=False)
+    rating = models.FloatField()
+    comment = models.CharField(max_length=750)
+
+    class Meta:
+        abstract = True
