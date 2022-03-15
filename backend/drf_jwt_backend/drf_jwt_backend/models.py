1755c4faa0c5129a9e983d6c565cba15e9aace0c
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-03-15 17:44:40 -0400

    feat: created Trip model

---
+++ backend/drf_jwt_backend/drf_jwt_backend/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Trip(models.Model):
+    driver = models.ForeignKey(User, on_delete=models.CASCADE)
+    departure_date = models.DateField()
+    arrival_date = models.DateField()
+    departure_time = models.DateTimeField(auto_now_add=True)
+    departure_city = models.CharField(max_length=100)
+    arrival_city = models.CharField(max_length=100)
+    available_seats = models.IntegerField()
+    seat_price = models.FloatField()
+
+    class Meta:
+        abstract = True
