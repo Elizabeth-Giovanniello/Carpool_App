769edbd1612635ab7cc5c9a47d380da157d5e303
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-03-15 17:47:03 -0400

    feat: created CheckIn model

--- backend/drf_jwt_backend/drf_jwt_backend/models.py
+++ backend/drf_jwt_backend/drf_jwt_backend/models.py
@@ -19,3 +19,13 @@ class TripPassenger(models.Model):
     passenger = models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
         abstract = True
+
+class CheckIn(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    class Meta:
+        abstract = True
