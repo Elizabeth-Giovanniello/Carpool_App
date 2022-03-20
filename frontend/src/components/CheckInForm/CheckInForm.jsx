b40c9cd397cbb23c283e764947923c6dd829824f
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-03-20 18:59:38 -0400

    feat: created check-in form and added geolocation so user's coordinates are added to check-in upon form submission

---
+++ frontend/src/components/CheckInForm/CheckInForm.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import useCustomForm from '../../hooks/useCustomForm';
+import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
+import { Button, Stack, TextField } from '@mui/material';
+
+
+const CheckInForm = (props) => {
+
+    const initialValues = {
+        description: "",
+        timestamp: null,
+    };
+
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+    const [finalFormData, setFinalFormData] = useState(initialValues);
+    const [hasLocationError, setHasLocationError] = useState(false);
+
+
+    const [user, token] = useAuth()
+    const navigate = useNavigate()
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues, checkIn)
+
+    async function checkIn(){
+        getCheckInLocation();
+        setFinalFormData({...formData, latitude: latitude, longitude: longitude})
+        try {
+            let response = await axios.post("INSERT CHECKIN PATH HERE", finalFormData, {
+                headers: {
+                    Authorization: 'Bearer' + token
+                }
+            })
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    function getCheckInLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                const pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+      
+                setLatitude(pos.lat);
+                setLongitude(pos.lng);
+              },
+              () => {
+                setHasLocationError(true);
+              }
+            );
+        } else {
+            setHasLocationError(true);
+        }
+    }
+
+    
+
+    console.log(formData);
+    return ( 
+        <Container>
+            <form onSubmit={handleSubmit}>
+                <Stack spacing={3}>
+                    //TODO: add logic for when it fails to grab location (like maybe have option to place pin on map appear)
+                    <TextField
+                        label="Leave a comment/description to help others find you"
+                        id="check-in-description"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleInputChange}
+                        sx={{ m: 1, width: '25ch' }}
+                    />
+                    <Button type="submit" variant="contained">Check in</Button>
+                </Stack>
+            </form>
+        </Container>
+    );
+}
+ 
+export default CheckInForm;
+
---
+++ frontend/src/components/TripInfo/TripInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TripInfo = (props) => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default TripInfo;
