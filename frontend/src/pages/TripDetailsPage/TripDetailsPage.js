8a97869139ac7be16d2809959166b433b8532693
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-03-20 21:49:46 -0400

    feat: added check-in urls to apiPaths page, created axios call to get all check-ins on trip details page.

--- frontend/src/pages/TripDetailsPage/TripDetailsPage.js
+++ frontend/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 import axios from "axios";
 import useAuth from "../../hooks/useAuth";
-import { rideDetailsPath } from "../../constants/apiPaths";
+import { getCheckInsPath, rideDetailsPath } from "../../constants/apiPaths";
 import Map, { MapContainer } from "../../components/Map/Map";
+import DisplayCheckIns from "../../components/DisplayCheckIns/DisplayCheckIns";
 
 const TripDetailsPage = () => {
+
+    const navigate = useNavigate()
+	const [user, token] = useAuth()
+	const [checkIns, setCheckIns] = useState([]);
 
     // async function fetchRideDetails(tripID) {
 	// 	let response = await axios.get(rideDetailsPath(tripID))
@@ -17,8 +23,32 @@ const TripDetailsPage = () => {
 	// 			console.log(error.response);
 	// 		});
 	// }
+
+	async function getCheckIns(tripID) {
+		let response = await axios.get(getCheckInsPath(tripID), {
+			headers: {
+				Authorization: 'Bearer' + token
+			}
+		})
+			.then(response => {
+				console.log(response);
+				setCheckIns(response);
+			})
+			.catch(error => {
+				console.log(error.response);
+			});
+	}
+
+	useEffect(() => {
+		getCheckIns(); //TODO: add functionality to pass in TripID
+	}, []);
+	
+	
     return ( 
-        <Map/>
+		<>
+			<Map checkIns={checkIns}/>
+			<DisplayCheckIns checkIns={checkIns} getCheckIns={getCheckIns}/>
+		</>
     );
 }
  
