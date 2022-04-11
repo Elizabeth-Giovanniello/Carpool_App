ce2c8574a7a9cb2bdb9c4b2dca9951c33bff8dbe
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-04-11 09:14:45 -0400

    feat: added image field to check-in model.

--- backend/drf_jwt_backend/check_ins/models.py
+++ backend/drf_jwt_backend/check_ins/models.py
@@ -9,4 +9,5 @@ class CheckIn(models.Model):
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='images/', null=True)
 
